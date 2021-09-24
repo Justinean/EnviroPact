b0e845c7ae3995caeb29fefc7723012029cd321b
Ody <melodymonyok@gmail.com>
2021-09-24 18:20:36 -0500

    Added mutation  typedefs.

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -9,7 +9,7 @@ const typeDefs = gql`
     savedCompanies: [Company]!
   }
   type Company {
-    esgId: ID
+    companyId: ID
     companyName: String
     exchangeSymbol: String
     stockSymbol: String
@@ -26,10 +26,29 @@ const typeDefs = gql`
     socialScore: Int
     governanceScore: Int
     total: Int
-    goals: [Goals]!
+    /* goals: [Goals]! */
   }
-  type Goals {
+  /* type Goals {
+    goalsId: ID
+    companyName: String
+    exchangeSymbol: String
+    stockSymbol: String
+    goals
+  } need to talk to Charlie about this one */
 
+  type Auth {
+    token: ID!
+    user: User
   }
-  
+
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    createUser(username: String!, email: String!, password: String!): Auth
+    followCompany(companyId: ID, companyName: String, exchangeSymbol: String, stockSymbol: String, environmentGrade: String, environmentLevel: String, socialGrade: String, socialLevel: String, governanceGrade: String, governanceLevel: String,totalGrade: String, totalLevel: String, lastProcessingDate: String, environmentScore: Int, socialScore: Int, governanceScore: Int, total: Int): User
+    unfollowCompany(companyId: ID): User
+  }
 `;
