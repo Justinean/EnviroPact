35618b816462e1822c5b6b9b18c10e69158b6db3
Justin Hoch <justinhoch22@gmail.com>
2021-09-25 10:11:08 -0500

    Added me query on server side

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,1 +1,10 @@
-// resolvers.js
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        me: async (parent, args, {user}) => {
+            return User.findOne({ _id: user._id}).populate('savedCompanies')
+        }
+    },
+}
