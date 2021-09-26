7cb224517894969d9c27e1af5065fbeec5fafa76
Heather Bjoin <h.m.bjoin@gmail.com>
2021-09-26 18:11:43 -0500

    Adding demo imported data to Company Chart

--- client/src/components/CompanyChart.js
+++ client/src/components/CompanyChart.js
@@ -4,11 +4,11 @@ import { Doughnut } from "react-chartjs-2";
 import { useQuery, useMutation } from '@apollo/client';
 import { GET_ME } from '../utils/queries';
 
-export default function CustomCharts() {
+export default function CustomCharts(props) {
 
-  const { loading, data } = useQuery(GET_ME);
-  const userData = data?.me || {};
 
+  const [score, setScore] = useState([])
+  score.push(props.data[0].environment_score, props.data[0].social_score, props.data[0].governance_score );
 
 
   const [chartData, setChartData] = useState({
@@ -21,7 +21,7 @@ export default function CustomCharts() {
 
     datasets: [{
       label: 'something',
-      data: [316, 339, 300],
+      data: score,
       backgroundColor: [
         '#415D43',
         '#CED0CE',
@@ -39,6 +39,7 @@ export default function CustomCharts() {
         width: 400,
         height: 200,
       }}>
+        <h1>{props.data[0].company_name}</h1>
         <Typography variant="h6">ESG Scores</Typography>
       <Doughnut
         data={chartData}
--- client/src/pages/landingPage/Landing.js
+++ client/src/pages/landingPage/Landing.js
@@ -11,21 +11,46 @@ import {
 
 import darkTheme from '../../assets/themes/DarkTheme'
 import './landing.css'
-const Landing = () => {
+import DataTable from '../../components/DataTable'
 
+
+
+const Landing = () => {
+  const data = [{
+  
+    "esg_id": 6909,
+    "company_name": "Target Corporation",
+    "exchange_symbol": "NYSE",
+    "stock_symbol": "TGT",
+    "environment_grade": "A",
+    "environment_level": "Medium",
+    "social_grade": "BBB",
+    "social_level": "Medium",
+    "governance_grade": "BBB",
+    "governance_level": "Medium",
+    "total_grade": "BBB",
+    "total_level": "Excellent",
+    "disclaimer": "ESG Enterprise's ESG Rating data (\"Scores\") are all based on public information and provided for informational purposes only. No member of ESG Enterprise or related parties make any prediction, warranty or representation whatsoever, expressly or impliedly, either as to the suitability of our Scores for any particular purposes or the validity of any derivative analysis or conclusion based on the Scores.",
+    "last_processing_date": "14-09-2021",
+    "environment_score": 500,
+    "social_score": 337,
+    "governance_score": 320,
+    "total": 1157
+  
+}]
 
 
   return (
     <ThemeProvider theme={darkTheme}>
     <Container className="main-container">
-      <Box className="box">
+      <Box sx={{ mr: 25, ml: 25 }} className="box">
         <Box className="card">
         <h3 className="enviro-header"> EnviroPact: </h3>
 
         <p className="enviro-para"> a pact to improve our environment, economy, and society by holding businesses, corporations, and governments accountable for their actions and damages. Our mission and goal at EnviroPact is to make information and data more accessible and transparent to the public so that together we can work towards creating a world with values.</p>
         </Box>
       </Box> 
-      <Box className="mission-box">
+      <Box sx={{ mr: 25, ml: 25 }} className="mission-box">
         <Box className="card">
         <h3 className="enviro-header"> Mission: </h3>
 
@@ -35,11 +60,18 @@ const Landing = () => {
       <Container className="chart-container">
 
       <Box className="chart-box">     
-      <h3 className="sample-chart-h3">Sample Chart</h3>
-      <CustomCharts />
+      <CustomCharts data={data}/>
       </Box>
       </Container>
+      <Container style={{marginTop: '300px'}}>
+        <Box>
+      <DataTable data={data}/>
+ 
+        </Box>
+      </Container>
+
     </Container>
+
     </ThemeProvider>
 
   );
