ae50e1162ecd652312fbc105740896072b940ae8
Justin Hoch <justinhoch22@gmail.com>
2021-09-26 14:30:45 -0500

    Removed the helpers i was using in the header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -6,35 +6,11 @@ import SignupForm from './SignupForm';
 import LoginForm from './LoginForm';
 
 export default function Header(props) {
-    const [[displaySignUp, setDisplaySignUp], [displayLogin, setDisplayLogin]] = [useState(false), useState(false)]
-
-    const showLogin = () => {
-        setDisplaySignUp(false)
-        setDisplayLogin(!displayLogin)
-    }
-
-    const showSignUp = () => {
-        setDisplayLogin(false)
-        setDisplaySignUp(!displaySignUp)
-    }
-
-    const logout = () => {
-        Auth.logout();
-    }
-
     return(
         <Container>
-            {Auth.loggedIn() ? 
-            <button onClick={logout}>Logout</button>
-            :
-            (
-            <div>
-                <button onClick={showLogin}>Login</button><button onClick={showSignUp}>Sign Up</button>
-            </div>
-            )
+            {
+                <h1>Hello from the header!</h1>
             }
-            {displaySignUp ? <SignupForm></SignupForm> : null}
-            {displayLogin ? <LoginForm></LoginForm> : null}
         </Container>
     )
 }
