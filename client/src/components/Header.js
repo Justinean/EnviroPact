9754e6d0ebd33fa76c3e221aa99f4cdfe96bc1c7
Justin Hoch <justinhoch22@gmail.com>
2021-09-26 14:23:39 -0500

    added signup functionality to client side

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import { Container } from '@mui/material';
+import Auth from '../utils/auth'
+import SignupForm from './SignupForm';
+import LoginForm from './LoginForm';
 
 export default function Header(props) {
+    const [[displaySignUp, setDisplaySignUp], [displayLogin, setDisplayLogin]] = [useState(false), useState(false)]
+
+    const showLogin = () => {
+        setDisplaySignUp(false)
+        setDisplayLogin(!displayLogin)
+    }
+
+    const showSignUp = () => {
+        setDisplayLogin(false)
+        setDisplaySignUp(!displaySignUp)
+    }
+
+    const logout = () => {
+        Auth.logout();
+    }
 
     return(
         <Container>
-            <h1>Hello from Header</h1>
+            {Auth.loggedIn() ? 
+            <button onClick={logout}>Logout</button>
+            :
+            (
+            <div>
+                <button onClick={showLogin}>Login</button><button onClick={showSignUp}>Sign Up</button>
+            </div>
+            )
+            }
+            {displaySignUp ? <SignupForm></SignupForm> : null}
+            {displayLogin ? <LoginForm></LoginForm> : null}
         </Container>
     )
 }
