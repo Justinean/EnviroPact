46506bd67682a2db21da252d261e4cbbe6f402a5
Justin Hoch <justinhoch22@gmail.com>
2021-09-26 14:33:14 -0500

    removed extra stuff from header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,16 +1,11 @@
-import React, {useState} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import { Container } from '@mui/material';
-import Auth from '../utils/auth'
-import SignupForm from './SignupForm';
-import LoginForm from './LoginForm';
 
 export default function Header(props) {
     return(
         <Container>
-            {
-                <h1>Hello from the header!</h1>
-            }
+            <h1>Hello from the header!</h1>
         </Container>
     )
 }
