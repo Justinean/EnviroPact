34c6416388a64d7471fde83a2bb517d2250016b6
Ody <melodymonyok@gmail.com>
2021-09-26 21:29:05 -0500

    Updated with search bar and the functionality for receiving data from Searchbar.

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,13 +1,20 @@
 // This is the initial page for the website aka (The home page?)
 
-import React from 'react';
+import React, { useState } from 'react';
 import CustomCharts from '../components/CompanyChart'
+import Searchbar from '../components/Searchbar';
 
 const Landing = () => {
+  const [data, setData] = useState('');
+  const passToParent = (childData) => {
+    setData(childData);
+  }
   return (
     <header className="">
       <div className="">
         <h1>This is the landing page</h1>
+        <p>This is the search query: {data}</p>
+        <Searchbar passToParent={passToParent}/>
         <CustomCharts />
       </div>
     </header>
