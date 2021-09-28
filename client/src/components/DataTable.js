c57183838209251f5394e6025849554ac047d5b3
Heather Bjoin <h.m.bjoin@gmail.com>
2021-09-27 21:59:04 -0500

    Finishing small dataTable style

--- client/src/components/DataTable.js
+++ client/src/components/DataTable.js
@@ -1,50 +1,85 @@
 import React, { useState } from "react";
-import { Typography, Container, Box } from "@material-ui/core";
-import { useQuery, useMutation } from '@apollo/client';
-import { GET_ME } from '../utils/queries';
+import { Box } from "@material-ui/core";
 import { Table, TableHead, TableBody, TableRow, TableCell, TableContainer } from '@mui/material'
+import { makeStyles } from '@mui/styles';
+
+
+
+const useStyles = makeStyles({
+    root: {
+        backgroundColor: '#CED0CE',
+        border: ' 2px solid #111D13',
+        color: '#111D13',
+    },
+    row: {
+        backgroundColor: '#CED0CE',
+        color: '#111D13',
+        '&:hover': {
+        backgroundColor: '#1E91D6',
+        color: 'snow'
+        }
+    },
+    cell: {
+        color: '#111D13',
+    },
+    box: {
+        marginBottom: '20px',
+    }
+})
+
+
 
 export default function DataTable(props) {
 
     function createData(enviroGrade, enviroScore, socialGrade, socialScore, govGrade, goveScore) {
         return { enviroGrade, enviroScore, socialGrade, socialScore, govGrade, goveScore };
     }
+    const classes = useStyles(props)
 
-    const rows = [
-        createData('')
-    ]
 
     return (
-        <TableContainer>
-            <h1>Hello from DataTable</h1>
-            <Table>
-                <TableHead>
-                    <h1>Company Name</h1>
-                    <TableRow style={{backgroundColor: 'orange'}}>
-                        <TableCell />
-                        <TableCell> Score </TableCell>
-                        <TableCell> Grade </TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    <TableRow>
-                        <TableCell>Enivormental</TableCell>
-                        <TableCell>A</TableCell>
-                        <TableCell>100</TableCell>
-                    </TableRow>
-                    <TableRow>
-                        <TableCell>Social</TableCell>
-                        <TableCell>A</TableCell>
-                        <TableCell>100</TableCell>
-                    </TableRow>
-                    <TableRow>
-                        <TableCell>Governance</TableCell>
-                        <TableCell>A</TableCell>
-                        <TableCell>100</TableCell>
-                    </TableRow>
+       
+            <Box sx={{
+        width: 400,
+        height: 200,
+        marginBottom: '20px',
+        color: '#111D13'
+      }}>
+            <TableContainer className={classes.root}>
+                <Table>
+                    <TableHead>
+                        <h1>Company Name</h1>
+                        <TableRow >
+                            <TableCell />
+                            <TableCell style={{
+                                color: '#111D13'
+                                }}> Score </TableCell>
+                            <TableCell style={{color: '#111D13'}}> Grade </TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        <TableRow className={classes.row} >
+                            <TableCell style={{color: '#111D13'}}>Environmental</TableCell>
+                            <TableCell style={{color: '#111D13'}}>A</TableCell>
+                            <TableCell style={{color: '#111D13'}}>100</TableCell>
+                        </TableRow>
+                        <TableRow className={classes.row}>
+                            <TableCell style={{color: '#111D13'}}>Social</TableCell>
+                            <TableCell style={{color: '#111D13'}}>A</TableCell>
+                            <TableCell style={{color: '#111D13'}}>100</TableCell>
+                        </TableRow>
+                        <TableRow className={classes.row}>
+                            <TableCell style={{color: '#111D13'}}>Governance</TableCell>
+                            <TableCell style={{color: '#111D13'}}>A</TableCell>
+                            <TableCell style={{color: '#111D13'}}>100</TableCell>
+                        </TableRow>
 
-                </TableBody>
-            </Table>
-        </TableContainer>
+                    </TableBody>
+                </Table>
+            </TableContainer>
+            </Box>
+           
+        
+        
     );
 }
