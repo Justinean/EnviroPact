4398ec278c461fe9472939174627338a2d5595df
Heather Bjoin <h.m.bjoin@gmail.com>
2021-09-30 23:33:43 -0500

    Adjusting FollowedCompanies layout and DataTable to float between landing and dashboard

--- client/src/components/DataTable.js
+++ client/src/components/DataTable.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { Box } from "@material-ui/core";
-import { Table, TableHead, TableBody, TableRow, TableCell, TableContainer } from '@mui/material'
+import { Table, TableHead, TableBody, TableRow, TableCell, TableContainer, List, ListItem, ListItemText } from '@mui/material'
 import { makeStyles } from '@mui/styles';
 
 const useStyles = makeStyles({
@@ -25,8 +25,107 @@ const useStyles = makeStyles({
   }
 })
 
-export default function DataTable({ data }) {
+export default function DataTable({ data, useFullData }) {
   const classes = useStyles(data)
+
+  const _renderStandardSetOfData = () => {
+    return (
+        <Table>
+          <TableHead>
+            {/* <h1>{data.companyName ? data.companyName : "Loading"}</h1> */}
+            <TableRow >
+              <TableCell />
+              <TableCell style={{
+                color: '#111D13'
+              }}> Grade </TableCell>
+              <TableCell style={{ color: '#111D13' }}> Level </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+          <TableRow className={classes.row} >
+          <TableCell style={{ color: '#111D13' }}>Environmental</TableCell>
+          <TableCell style={{ color: '#111D13' }}>{data.environmentGrade ? data.environmentGrade : 0}</TableCell>
+          <TableCell style={{ color: '#111D13' }}>{data.environmentScore ? data.environmentScore : 0}</TableCell>
+        </TableRow>
+        <TableRow className={classes.row}>
+          <TableCell style={{ color: '#111D13' }}>Social</TableCell>
+          <TableCell style={{ color: '#111D13' }}>{data.socialGrade ? data.socialGrade : 0}</TableCell>
+          <TableCell style={{ color: '#111D13' }}>{data.socialScore ? data.socialScore : 0}</TableCell>
+        </TableRow>
+        <TableRow className={classes.row}>
+          <TableCell style={{ color: '#111D13' }}>Governance</TableCell>
+          <TableCell style={{ color: '#111D13' }}>{data.governanceGrade ? data.governanceGrade : 0}</TableCell>
+          <TableCell style={{ color: '#111D13' }}>{data.governanceScore ? data.governanceScore : 0}</TableCell>
+        </TableRow>
+          </TableBody>
+        </Table>
+    );
+  }
+
+  const _renderFullSetOfData = () => {
+    const {
+      companyId,
+      companyName,
+      exchangeSymbol,
+      stockSymbol,
+      environmentGrade,
+      environmentLevel,
+      environmentScore,
+      socialGrade,
+      socialScore,
+      socialLevel,
+      governanceGrade,
+      governanceLevel,
+      governanceScore,
+      total,
+      totalGrade,
+      totalLevel,
+      lastProcessingDate
+    } = data;
+
+    return (
+      <Table>
+          <TableHead>
+          <p>{companyId} - {companyName} - {exchangeSymbol}/{stockSymbol} [{lastProcessingDate}]</p>
+            <TableRow >
+              <TableCell />
+              <TableCell style={{
+                color: '#111D13'
+              }}> Grade </TableCell>
+              <TableCell style={{ color: '#111D13' }}> Level </TableCell>
+              <TableCell style={{ color: '#111D13' }}> Score </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <TableRow className={classes.row} >
+            <TableCell style={{ color: '#111D13' }}>Environmental</TableCell>
+            <TableCell style={{ color: '#111D13' }}>{environmentGrade ? environmentGrade : 0}</TableCell>
+            <TableCell style={{ color: '#111D13' }}>{environmentLevel ? environmentLevel : 0}</TableCell>
+            <TableCell style={{ color: '#111D13' }}>{environmentScore ? environmentScore : 0}</TableCell>
+          </TableRow>
+          <TableRow className={classes.row} >
+            <TableCell style={{ color: '#111D13' }}>Social</TableCell>
+            <TableCell style={{ color: '#111D13' }}>{socialGrade ? socialGrade : 0}</TableCell>
+            <TableCell style={{ color: '#111D13' }}>{socialLevel ? socialLevel : 0}</TableCell>
+            <TableCell style={{ color: '#111D13' }}>{socialScore ? socialScore : 0}</TableCell>
+          </TableRow>
+          <TableRow className={classes.row} >
+            <TableCell style={{ color: '#111D13' }}>Governance</TableCell>
+            <TableCell style={{ color: '#111D13' }}>{governanceGrade ? governanceGrade : 0}</TableCell>
+            <TableCell style={{ color: '#111D13' }}>{governanceLevel ? governanceLevel : 0}</TableCell>
+            <TableCell style={{ color: '#111D13' }}>{governanceScore ? governanceScore : 0}</TableCell>
+          </TableRow>
+          <TableRow className={classes.row} >
+            <TableCell style={{ color: '#111D13' }}>Total</TableCell>
+            <TableCell style={{ color: '#111D13' }}>{totalGrade ? totalGrade : 0}</TableCell>
+            <TableCell style={{ color: '#111D13' }}>{totalLevel ? totalLevel : 0}</TableCell>
+            <TableCell style={{ color: '#111D13' }}>{total ? total : 0}</TableCell>
+          </TableRow>
+          </TableBody>
+        </Table>
+    )
+  }
+
 
   return (
 
@@ -39,41 +138,8 @@ export default function DataTable({ data }) {
       color: '#111D13'
     }}>
       <TableContainer className={classes.root}>
-        <Table>
-          <TableHead>
-            {/* <h1>{data.companyName ? data.companyName : "Loading"}</h1> */}
-            <TableRow >
-              <TableCell />
-              <TableCell style={{
-                color: '#111D13'
-              }}> Score </TableCell>
-              <TableCell style={{ color: '#111D13' }}> Grade </TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            <TableRow className={classes.row} >
-              <TableCell style={{ color: '#111D13' }}>Environmental</TableCell>
-              <TableCell style={{ color: '#111D13' }}>{data.environmentGrade ? data.environmentGrade : 0}</TableCell>
-              <TableCell style={{ color: '#111D13' }}>{data.environmentScore ? data.environmentScore : 0}</TableCell>
-            </TableRow>
-            <TableRow className={classes.row}>
-              <TableCell style={{ color: '#111D13' }}>Social</TableCell>
-              <TableCell style={{ color: '#111D13' }}>{data.socialGrade ? data.socialGrade : 0}</TableCell>
-              <TableCell style={{ color: '#111D13' }}>{data.socialScore ? data.socialScore : 0}</TableCell>
-            </TableRow>
-            <TableRow className={classes.row}>
-              <TableCell style={{ color: '#111D13' }}>Governance</TableCell>
-              <TableCell style={{ color: '#111D13' }}>{data.governanceGrade ? data.governanceGrade : 0}</TableCell>
-              <TableCell style={{ color: '#111D13' }}>{data.governanceScore ? data.governanceScore : 0}</TableCell>
-            </TableRow>
-
-          </TableBody>
-        </Table>
+        {useFullData ? _renderFullSetOfData() : _renderStandardSetOfData() }
       </TableContainer>
     </Box>
-  
-
-
-
   );
 }
--- client/src/components/FollowedCompanies.js
+++ client/src/components/FollowedCompanies.js
@@ -2,14 +2,19 @@ import React from 'react';
 import {
   // Box,
   // Button,
+  Accordion,
+  AccordionSummary,
   Container,
   IconButton,
+  AccordionDetails,
   List,
   ListItem,
   ListItemText,
 } from '@mui/material';
+import Typography from '@mui/material/Typography';
 import DeleteIcon from '@mui/icons-material/Delete';
 import SearchIcon from '@mui/icons-material/Search';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { GET_ME } from '../utils/queries';
 import { useQuery, useMutation } from '@apollo/client';
 import { UNFOLLOW_COMPANY } from '../utils/mutations';
@@ -17,18 +22,78 @@ import { removeCompanyId } from '../utils/localStorage';
 import Auth from '../utils/auth';
 import { APIClickable } from './APIClickable';
 
+// const FollowedCompanies = (props) => {
+//   const { data, loading } = useQuery(GET_ME);
+//   const userData = data?.me || {};
+//   const [unfollowCompany] = useMutation(UNFOLLOW_COMPANY);
+  
+
+//   /* const viewData = (e, value) => {
+//     const data = [e.currentTarget.value];
+//     console.log("viewData: ", data);
+//     passViewCompany(data);
+//   }; */
+
+//   const onUnfollow = async companyId => {
+//     if (!Auth.loggedIn()) {
+//       return;
+//     }
+//     await unfollowCompany({ variables: { companyId } });
+//     removeCompanyId(companyId);
+//   }
+
+//   const _renderCompanies = (item, index) => {
+//     const setIndexandSend = () => {
+//       onUnfollow(JSON.parse(localStorage.getItem('followedCompanies'))[index])
+//     }
+//     return (
+//       // inside here will go the list items
+//       <ListItem
+//         secondaryAction={
+//           <IconButton
+//             edge="end"
+//             aria-label="delete"
+//             onClick={setIndexandSend}>
+//             <DeleteIcon />
+//           </IconButton>
+//         }
+//         key={item.companyId}>
+//           <APIClickable query={item.stockSymbol}
+//           ><SearchIcon /></APIClickable>
+//         {/* <IconButton
+//           edge="start"
+//           aria-label="search"
+//           // onClick={viewData}
+//           value={item.stockSymbol}
+//           style={{ color: 'black' }}>
+          
+//         </IconButton> */}
+//         <ListItemText
+//           primary={item.companyName}
+//           // secondary="View Data"
+//           >
+//         </ListItemText>
+//         {/* <Button onClick={viewData} data={item.stockSymbol} style={{ color: 'black' }}> View Data </Button> */}
+//       </ListItem>
+//     )
+//   };
+
+//   return (
+//     <Container style={{ backgroundColor: '#1E91D6', color: 'white' }}>
+//       <h2>Followed Companies</h2>
+//       <List>
+//         {loading ? <span></span> : userData.followedCompanies.map(_renderCompanies)}
+//       </List>
+//     </Container>
+//   )
+// }
+
+
 const FollowedCompanies = (props) => {
   const { data, loading } = useQuery(GET_ME);
   const userData = data?.me || {};
   const [unfollowCompany] = useMutation(UNFOLLOW_COMPANY);
   
-
-  /* const viewData = (e, value) => {
-    const data = [e.currentTarget.value];
-    console.log("viewData: ", data);
-    passViewCompany(data);
-  }; */
-
   const onUnfollow = async companyId => {
     if (!Auth.loggedIn()) {
       return;
@@ -41,8 +106,8 @@ const FollowedCompanies = (props) => {
     const setIndexandSend = () => {
       onUnfollow(JSON.parse(localStorage.getItem('followedCompanies'))[index])
     }
+
     return (
-      // inside here will go the list items
       <ListItem
         secondaryAction={
           <IconButton
@@ -55,31 +120,27 @@ const FollowedCompanies = (props) => {
         key={item.companyId}>
           <APIClickable query={item.stockSymbol}
           ><SearchIcon /></APIClickable>
-        {/* <IconButton
-          edge="start"
-          aria-label="search"
-          // onClick={viewData}
-          value={item.stockSymbol}
-          style={{ color: 'black' }}>
-          
-        </IconButton> */}
         <ListItemText
           primary={item.companyName}
-          // secondary="View Data"
-          >
+        >
         </ListItemText>
-        {/* <Button onClick={viewData} data={item.stockSymbol} style={{ color: 'black' }}> View Data </Button> */}
       </ListItem>
     )
   };
 
   return (
-    <Container style={{ backgroundColor: '#1E91D6', color: 'white' }}>
-      <h2>Followed Companies</h2>
-      <List>
+    <Accordion>
+      <AccordionSummary
+        expandIcon={<ExpandMoreIcon />}
+        aria-controls="panel1a-content"
+        id="panel1a-header"
+      >
+        <Typography>Followed Companies</Typography>
+      </AccordionSummary>
+      <AccordionDetails>
         {loading ? <span></span> : userData.followedCompanies.map(_renderCompanies)}
-      </List>
-    </Container>
+      </AccordionDetails>
+    </Accordion>
   )
 }
 
