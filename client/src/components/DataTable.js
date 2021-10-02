30bb6a4f1e0ade9243b7a3bdbf94ddfa45f3a9bd
Heather Bjoin <h.m.bjoin@gmail.com>
2021-10-01 20:10:36 -0500

    Finishing data table

--- client/src/components/DataTable.js
+++ client/src/components/DataTable.js
@@ -1,6 +1,8 @@
 import React, { Fragment } from "react";
+import PropTypes from 'prop-types';
+import { useTheme } from '@mui/material/styles';
 import { Box } from "@material-ui/core";
-import { Table, TableHead, TableBody, TableRow, TableCell, TableContainer, List, ListItem, ListItemText } from '@mui/material'
+import { Table, TableHead, TableBody, TableRow, TableCell, TableContainer, TablePagination, } from '@mui/material'
 import { makeStyles } from '@mui/styles';
 
 const useStyles = makeStyles({
@@ -8,6 +10,7 @@ const useStyles = makeStyles({
     backgroundColor: '#CED0CE',
     border: ' 2px solid #111D13',
     color: '#111D13',
+    maxHeight: 440,
   },
   row: {
     backgroundColor: '#CED0CE',
@@ -22,24 +25,15 @@ const useStyles = makeStyles({
   },
   box: {
     marginBottom: '20px',
-  },
-  tableBox:{
-    padding:'6px',
-    width: '400px',
-    marginTop: '85px',
-    marginBottom: '30px',
-    color: '#111D13'
-}
+  }
 })
 
 export default function DataTable({ data, useFullData }) {
   const classes = useStyles(data)
 
-
   const _renderStandardSetOfData = () => {
     return (
-
-        <Table>
+        <Table >
           <TableHead>
             {/* <h1>{data.companyName ? data.companyName : "Loading"}</h1> */}
             <TableRow >
@@ -93,42 +87,115 @@ export default function DataTable({ data, useFullData }) {
     } = data;
 
     return (
-      <Table>
+      <Table sx={{maxHeight: '4%'}}>
           <TableHead>
-          <p>{companyId} - {companyName} - {exchangeSymbol}/{stockSymbol} [{lastProcessingDate}]</p>
+            <p>Last update on: </p>
+            <p style={{color: '#1E91D6'}}>[{lastProcessingDate}]</p>
             <TableRow >
               <TableCell />
               <TableCell style={{
                 color: '#111D13'
-              }}> Grade </TableCell>
-              <TableCell style={{ color: '#111D13' }}> Level </TableCell>
-              <TableCell style={{ color: '#111D13' }}> Score </TableCell>
+              }}></TableCell>
+              <TableCell style={{ color: '#111D13' }}></TableCell>
+              <TableCell style={{ color: '#111D13' }}> </TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
+          <TableRow className={classes.row} >
+            <TableCell style={{ color: '#111D13' }}>Company Id</TableCell>
+            <TableCell />       
+            <TableCell style={{ color: '#111D13' }}>{companyId ? companyId : 0}</TableCell>
+            <TableCell /> 
+          </TableRow>
+          <TableRow className={classes.row} >
+            <TableCell style={{ color: '#111D13' }}>Company Name</TableCell>
+            <TableCell />       
+            <TableCell style={{ color: '#111D13' }}>{companyName ? companyName : 0}</TableCell>
+            <TableCell /> 
+          </TableRow>
+          <TableRow className={classes.row} >
+            <TableCell style={{ color: '#111D13' }}>Exchange Symbol</TableCell>
+            <TableCell />       
+            <TableCell style={{ color: '#111D13' }}>{exchangeSymbol ? exchangeSymbol : 0}</TableCell>
+            <TableCell /> 
+          </TableRow>
+          <TableRow className={classes.row} >
+            <TableCell style={{ color: '#111D13' }}>Stock Symbol</TableCell>
+            <TableCell />       
+            <TableCell style={{ color: '#111D13' }}>{stockSymbol ? stockSymbol : 0}</TableCell>
+            <TableCell /> 
+          </TableRow>
             <TableRow className={classes.row} >
-            <TableCell style={{ color: '#111D13' }}>Environmental</TableCell>
+            <TableCell style={{ color: '#111D13' }}>Environmental Grade</TableCell>
+            <TableCell />       
             <TableCell style={{ color: '#111D13' }}>{environmentGrade ? environmentGrade : 0}</TableCell>
+            <TableCell /> 
+          </TableRow>
+          <TableRow className={classes.row} >
+            <TableCell style={{ color: '#111D13' }}>Environmental Level</TableCell>
+            <TableCell />
             <TableCell style={{ color: '#111D13' }}>{environmentLevel ? environmentLevel : 0}</TableCell>
+            <TableCell />
+          </TableRow>
+          <TableRow className={classes.row} >
+            <TableCell style={{ color: '#111D13' }}>Environmental Score</TableCell>
+            <TableCell />
             <TableCell style={{ color: '#111D13' }}>{environmentScore ? environmentScore : 0}</TableCell>
+            <TableCell />
           </TableRow>
           <TableRow className={classes.row} >
-            <TableCell style={{ color: '#111D13' }}>Social</TableCell>
+            <TableCell style={{ color: '#111D13' }}>Social Grade</TableCell>
+            <TableCell />
             <TableCell style={{ color: '#111D13' }}>{socialGrade ? socialGrade : 0}</TableCell>
+            <TableCell />
+          </TableRow>
+          <TableRow className={classes.row} >
+            <TableCell style={{ color: '#111D13' }}>Social Level</TableCell>
+            <TableCell />
             <TableCell style={{ color: '#111D13' }}>{socialLevel ? socialLevel : 0}</TableCell>
+            <TableCell />
+          </TableRow>
+          <TableRow className={classes.row} >
+            <TableCell style={{ color: '#111D13' }}>Social Score</TableCell>
+            <TableCell />
             <TableCell style={{ color: '#111D13' }}>{socialScore ? socialScore : 0}</TableCell>
+            <TableCell />
           </TableRow>
           <TableRow className={classes.row} >
-            <TableCell style={{ color: '#111D13' }}>Governance</TableCell>
+            <TableCell style={{ color: '#111D13' }}>Governance Grade</TableCell>
+            <TableCell />
             <TableCell style={{ color: '#111D13' }}>{governanceGrade ? governanceGrade : 0}</TableCell>
-            <TableCell style={{ color: '#111D13' }}>{governanceLevel ? governanceLevel : 0}</TableCell>
-            <TableCell style={{ color: '#111D13' }}>{governanceScore ? governanceScore : 0}</TableCell>
+            <TableCell />
           </TableRow>
           <TableRow className={classes.row} >
-            <TableCell style={{ color: '#111D13' }}>Total</TableCell>
+            <TableCell style={{ color: '#111D13' }}>Governance Level</TableCell>
+            <TableCell />
+            <TableCell style={{ color: '#111D13' }}>{governanceLevel ? governanceLevel : 0}</TableCell>
+            <TableCell />
+          </TableRow>
+          <TableRow className={classes.row} >
+            <TableCell style={{ color: '#111D13' }}>Governance Score</TableCell>
+            <TableCell />
+            <TableCell style={{ color: '#111D13' }}>{governanceScore ? governanceScore : 0}</TableCell>
+            <TableCell />
+          </TableRow>
+          <TableRow className={classes.row} >
+            <TableCell style={{ color: '#111D13' }}>Comprehensive Grade</TableCell>
+            <TableCell />
             <TableCell style={{ color: '#111D13' }}>{totalGrade ? totalGrade : 0}</TableCell>
+            <TableCell />
+          </TableRow>
+          <TableRow className={classes.row} >
+            <TableCell style={{ color: '#111D13' }}>Comprehensive Level</TableCell>
+            <TableCell />
             <TableCell style={{ color: '#111D13' }}>{totalLevel ? totalLevel : 0}</TableCell>
-            <TableCell style={{ color: '#111D13' }}>{total ? total : 0}</TableCell>
+            <TableCell />
+          </TableRow>
+          <TableRow className={classes.row} >
+            <TableCell style={{ color: '#111D13' }}>Comprehensive Score</TableCell>
+            <TableCell />
+            <TableCell style={{ color: '#111D13' }}>{totalGrade ? totalGrade : 0}</TableCell>
+            <TableCell />
           </TableRow>
           </TableBody>
         </Table>
@@ -137,7 +204,15 @@ export default function DataTable({ data, useFullData }) {
 
 
   return (
-    <Box className={classes.tableBox}>
+
+    <Box sx={{
+      width: 400,
+      // height: 200,
+      // marginLeft: '50px',
+      marginTop: '85px',
+      marginBottom: '30px',
+      color: '#111D13'
+    }}>
       <TableContainer className={classes.root}>
         {useFullData ? _renderFullSetOfData() : _renderStandardSetOfData() }
       </TableContainer>
